2fd102e783a19a0b8674a0d73245b818e855efaa
meganbinondo <meganbinondo@gmail.com>
2023-11-20 02:14:39 +0800

    Added Store functionality in CarouselItemsController

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\CarouselItemsRequest;
 use App\Models\CarouselItems;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -20,9 +21,14 @@ class CarouselItemsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CarouselItemsRequest $request)
     {
-        //
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $CarouselItem = CarouselItems::create($validated);
+
+        return $CarouselItem;
     }
 
     /**
