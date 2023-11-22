e50709f775fe12f16b6637bea7dec7fd95b2bac9
meganbinondo <meganbinondo@gmail.com>
2023-11-22 16:24:25 +0800

    Added AuthController and Login Function with API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CarouselItemsController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,21 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login',                  'login')    ->name('user.login');
+    Route::post('/logout',                 'logout')   ->name('user.logout');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::controller(CarouselItemsController::class)->group(function () {
+    Route::get('/carousel',                 'index');
+    Route::get('/carousel/{id}',            'show');
+    Route::post('/carousel',                'store');
+    Route::put('/carousel/{id}',            'update');
+    Route::delete('/carousel/{id}',         'destroy');
+});
 
-Route::get('/carousel', [CarouselItemsController::class, 'index']);
-Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
-Route::post('/carousel', [CarouselItemsController::class, 'store']);
-Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
-Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/{id}', [UserController::class, 'show']);
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
-Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
-Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
-Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
-Route::delete('/user/{id}', [UserController::class, 'destroy']);
+
+
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user/{id}', [UserController::class, 'show']);
+// Route::post('/user', [UserController::class, 'store'])->name('user.store');
+// Route::put('/user/{id}', [UserController::class, 'update'])->name('user.update');
+// Route::put('/user/email/{id}', [UserController::class, 'email'])->name('user.email');
+// Route::put('/user/password/{id}', [UserController::class, 'password'])->name('user.password');
+// Route::delete('/user/{id}', [UserController::class, 'destroy']);
